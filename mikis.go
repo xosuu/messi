f6f731b9fbf4f0b69dbafbb0be3173f931abc0a5
xosuu <fortestmanchoso@gmail.com>
2026-02-17 18:11:50 +0000

    uu

---
+++ mikis.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main(){
+	//var s string 
+	var nombre string
+	fmt.Print("Tu nombre: ")
+	fmt.Scanln(&nombre)
+
+	fmt.Println("Hola "+nombre)
+}
+
+
+
+
+
